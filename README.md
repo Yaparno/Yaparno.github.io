909620f3eb22c4f6eeec8c295f1b827f6a4d7ba4
Yaparno <yaparno15@mhs.unimed.ac.id>
2023-11-30 12:17:30 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Yaparno.github.io
---
+++ story_content/user.js
@@ -0,0 +1,455 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5ermwY3ytrf":
+        Script1();
+        break;
+      case "5ynrv5ZiLFL":
+        Script2();
+        break;
+      case "6TRrvE7rSfR":
+        Script3();
+        break;
+      case "5vyhZbxQCpy":
+        Script4();
+        break;
+      case "6UQkKONCRLH":
+        Script5();
+        break;
+      case "6jTQVogA0M0":
+        Script6();
+        break;
+      case "5jSd78jcVXK":
+        Script7();
+        break;
+      case "5bPI74bj4fB":
+        Script8();
+        break;
+      case "6QMC7S1KEX7":
+        Script9();
+        break;
+      case "6pKH8qHqMf8":
+        Script10();
+        break;
+      case "5ugqpeoS9mx":
+        Script11();
+        break;
+      case "6fiPqvxJlIa":
+        Script12();
+        break;
+      case "5jYNKlwQNTK":
+        Script13();
+        break;
+      case "5iYXhA0esDS":
+        Script14();
+        break;
+      case "68qAUbVEYnp":
+        Script15();
+        break;
+      case "5VFLLqXvLxZ":
+        Script16();
+        break;
+      case "6O9SqonxNpw":
+        Script17();
+        break;
+      case "6hmQxgggCrc":
+        Script18();
+        break;
+      case "5iNnGjVWspS":
+        Script19();
+        break;
+      case "5dFTjuNkwXS":
+        Script20();
+        break;
+      case "6Xz0CmC5qG0":
+        Script21();
+        break;
+      case "5WALV0MCM1M":
+        Script22();
+        break;
+      case "6X6O1quMXQ6":
+        Script23();
+        break;
+      case "68hywINEs7n":
+        Script24();
+        break;
+      case "6eO5CIzqvy6":
+        Script25();
+        break;
+      case "5xgw2iuIbuX":
+        Script26();
+        break;
+      case "5ZtD9HQOGI0":
+        Script27();
+        break;
+      case "6JKbgch8Aqd":
+        Script28();
+        break;
+      case "6h39kjXQs2F":
+        Script29();
+        break;
+      case "6Oo4SiMyPQW":
+        Script30();
+        break;
+      case "5iZ1H8cEOu8":
+        Script31();
+        break;
+      case "5oeIZxDyFof":
+        Script32();
+        break;
+  }
+}
+
+function Script1()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script2()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script3()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script4()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script5()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script6()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script7()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script8()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script9()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script10()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script11()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script12()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script13()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script14()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script15()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script16()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script17()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script18()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script19()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script20()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script21()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script22()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script23()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script24()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script25()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script26()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script27()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script28()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script29()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script30()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script31()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script32()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
